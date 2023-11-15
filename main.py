4d2dc9ee9bd51671aae08c99a88ddf60379b6951
abhits <abhiramits004@gmail.com>
2023-11-15 13:42:26 +0530

    Update dot placement logic with penup/pendown

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ square_size = 100
 timmy.penup()
 timmy.goto(-screen.window_width() / 2 + square_size / 2, -screen.window_height() / 2 + square_size / 2)
 timmy.pendown()
-
+timmy.speed("fastest")
 # Calculate the maximum x-coordinate for the turtle to move before wrapping to the next row
 max_x = screen.window_width() / 2 - square_size / 2
+
 
 # Function to extract colors from an image and store them in the color_list
 def extract_color():
@@ -37,15 +38,18 @@ def extract_color():
         color_list.append((color.rgb.r/255, color.rgb.g/255, color.rgb.b/255))
     return color_list
 
+
 # Call the function to extract colors and store them in list_of_colors
 list_of_colors = extract_color()
 
 # Loop to create a pattern using dots with random colors
 for i in range(len(list_of_colors)//2):
     for j in range(len(list_of_colors) // 2):
+        timmy.pendown()
         # Draw a dot with a random color from the list
         timmy.dot(20, random.choice(list_of_colors))
         # Move the turtle forward by 50 units
+        timmy.penup()
         timmy.forward(50)
 
     # Adjust the turtle's position based on the row index
